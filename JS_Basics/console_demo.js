241abbe44c890d16ba6884fec6d1139cda5f101f
Dipin Grover <grover.dipin007@gmail.com>
2025-07-06 18:31:41 +0530

    added 2 files of JS

---
+++ JS_Basics/console_demo.js
@@ -0,0 +1,10 @@
+// second js file 
+
+console.log(10);
+console.log("Hello World!");
+
+
+var age = 21;
+console.log(age);
+
+console.log("Randomtext" , 6.78,age);
---
+++ JS_Basics/variable_demo.js
@@ -0,0 +1,12 @@
+// first JS file 
+
+var score  = 10;
+var marks = 58;
+
+
+let age = 22;
+let flag = 7;
+
+const x = 13;
+const y = 67;
+
