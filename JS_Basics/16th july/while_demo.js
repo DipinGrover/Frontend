ce37178526d8a010c3bfde08b8503d329b7f2c52
Dipin Grover <grover.dipin007@gmail.com>
2025-08-06 23:16:04 +0530

    started JS on 6th aug

--- JS_Basics/16th july/while_demo.js
+++
@@ -1,8 +0,0 @@
-let i = 1;
-while(i <= 10)
-{
-    console.log(i);
-    i++;
-}
-
-console.log("End of Program");
--- JS_Basics/Problems/odd_even.js
+++
@@ -1,9 +0,0 @@
-let x = 79;
-
-if(x % 2 == 0)
-{
-    console.log("Even");
-}
-else{
-    console.log("Odd");
-}
