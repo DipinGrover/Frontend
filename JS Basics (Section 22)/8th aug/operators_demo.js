477d16e3ad7e64a075ae3154adf68a5c34fddaf3
Dipin Grover <grover.dipin007@gmail.com>
2025-08-08 20:10:13 +0530

    added till 8th aug

---
+++ JS Basics (Section 22)/8th aug/operators_demo.js
@@ -0,0 +1,24 @@
+// Relational operators
+let x = 10;
+let y = 20;
+let z = 5;
+let a = 10;
+
+console.log("x < z" , x<z);
+console.log("y > z" , y>z);
+console.log("x <= z" , x<=z);
+console.log("x >= a" , x>=a);
+
+
+// Logical operators 
+console.log(true && false);
+console.log(false || false);
+console.log(true && true);
+console.log((5<10) && (6<3));
+console.log(!false);
+
+// short circuiting
+console.log(6 && 7);
+console.log(0 && -11);
+console.log(4 || 8);
+console.log(0 ||-13); 
